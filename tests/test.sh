dcfabf40a55f23bbb1804aed436bf337ad5b5183
kimpure <gimleni593@gmail.com>
2025-01-21 14:19:47 +0000

    .gitignore

--- tests/test.sh
+++ tests/test.sh
@@ -1,15 +1,3 @@
-# luau project fast setup
-
-[tests](./tests/test.sh)
-
-```bash
 pesde x kimpure/project_fast_setup
-```
-
-```bash
 pesde x kimpure/project_fast_setup -- --lune
-```
-
-```bash
 pesde x kimpure/project_fast_setup -- --roblox
-```
